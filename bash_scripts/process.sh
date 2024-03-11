ff0012cf7d977cfce3f539003ebb56a69323dbee
George Sotirchos <gsotirchos@login01.delftblue.tudelft.nl>
2024-03-11 09:22:30 +0100

    Fix naming typo for automatic database extraction & merging

--- bash_scripts/process.sh
+++ bash_scripts/process.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-prefix="package://pyre/datasets"
+
+prefix_dset="package://pyre/datasets"
+prefix_db="package://pyre/database"
 dataset=("shelf_zero" "shelf_height" "shelf_height_rot")   
 
 for d in ${dataset[*]};
 do 
-    dset=$prefix/$d/
-    roslaunch --wait pyre process.launch dataset:=$dset start:=1 end:=100 & 
-    roslaunch --wait pyre process.launch dataset:=$dset start:=101 end:=200 & 
-    roslaunch --wait pyre process.launch dataset:=$dset start:=201 end:=300 &
-    roslaunch --wait pyre process.launch dataset:=$dset start:=301 end:=400 &
-    roslaunch --wait pyre process.launch dataset:=$dset start:=401 end:=500 &
+    dset=$prefix_dset/$d/
+    dbase=$prefix_db/$d/
+
+    roslaunch --wait pyre process.launch dataset:=$dset database:=$dbase start:=1 end:=100 & 
+    roslaunch --wait pyre process.launch dataset:=$dset database:=$dbase start:=101 end:=200 & 
+    roslaunch --wait pyre process.launch dataset:=$dset database:=$dbase start:=201 end:=300 &
+    roslaunch --wait pyre process.launch dataset:=$dset database:=$dbase start:=301 end:=400 &
+    roslaunch --wait pyre process.launch dataset:=$dset database:=$dbase start:=401 end:=500 &
     wait
 done
